1e489fa0328623b2ccc5b3f7551405a94ba339fe
Jen Gori <114230832+jengori@users.noreply.github.com>
2023-10-05 12:51:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ in the terminal
 Visit localhost:8000 in your web browser, and you should see the message:
 "Hello! You have been": "short changed"
 
-In database.py file, replace values of password (lines 18 and 19) with your own MySQL password
+In database.py file, replace values of password (lines 10 and 18) with your own MySQL password
 
 Run the main.py file
 """
